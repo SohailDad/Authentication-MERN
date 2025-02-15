386e101d70bb589b0fad42d8b8d7660749704b6c
SohailDAD <dsohail866@gmail.com>
2025-02-16 00:47:17 +0500

    almost backend complete

--- backend/index.js
+++ backend/index.js
@@ -3,23 +3,35 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-const { MONGO_URL, PORT } = process.env ;
-
-app.use(express.json());
+const cookieParser = require("cookie-parser");
+const authRoute = require("./Routes/AuthRoute");
+const MONGO_URL = process.env.MONGO_URL;
+const PORT = 3000
 
 mongoose
   .connect(MONGO_URL)
-  .then(() => console.log("MongoDB is connected successfully"))
-  .catch((err) => console.error("MongoDB connection error:", err));
-
-app.listen(3000, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  .then(() => console.log("MongoDB is  connected successfully"))
+  .catch((err) =>{ 
+    console.error("Error:", err)
+    process.exit(1);
 });
 
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+app.use("/", authRoute);
+
 app.use(
-  cors({
-    origin: ["http://localhost:4000"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+    cors({
+      origin: ["http://localhost:4000"],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    })
+  );
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
